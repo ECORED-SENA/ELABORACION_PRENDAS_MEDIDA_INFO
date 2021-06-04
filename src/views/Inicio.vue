2f26f8e8adbc9b72d0220273f1cbde8e74c9986c
Luis Gabriel Urueta Alvarez <lgurueta@misena.edu.co>
2021-06-04 17:11:44 -0500

    avance1

---
+++ src/views/Inicio.vue
@@ -0,0 +1,148 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p El programa técnico en Elaboración de prendas de vestir sobre medidas está orientado hacia la interpretación de modelos o diseños para iniciar el trabajo de confección, trazar y transformar moldes, cortar prendas de diferentes características y operar máquinas industriales para realizar el ensamble y acabados de todo tipo de vestuario sobre medidas con diferentes grados de complejidad, a su vez fomenta en el aprendiz el emprendimiento que le permitirá conducirse hacia la formalidad de este oficio y por ende, mejorar su calidad de vida.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+        
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+  
+    p.mb-4 El 2020 generó a nivel mundial una serie de transformaciones en el comportamiento social, económico, laboral y educativo por la pandemia del Covid-19, derivando escenarios de emprendimiento de economía naranja y, adaptación de los modelos de negocio de PYMES y de empresas que lograron sobrevivir ante la crisis. 
+    p.mb-4 En consecuencia, el SENA interviene no solo como principal entidad de formación para el trabajo sino como un gestor de transformación social, cuyo objetivo es apoyar la activación post-covid en el sector confección que afectó desproporcionadamente a las minorías y a aquellos en posición de vulnerabilidad. A este último grupo pertenece la gran mayoría de modistas, costureros y sastres de nuestro país —principalmente mujeres— que si antes de este fenómeno ya presentaban problemas para acceder a créditos por su informalidad laboral (Unabradio, 2019) hoy necesitan capacitarse para mejorar sus ingresos y pertenecer a un marco legal. 
+    p.mb-4 Con este programa se busca llegar al grupo invisible en la cadena productiva que no cuenta con registros en la Cámara de Comercio o ante los clúster; a mujeres cabezas de familia que literalmente han convertido sus hogares en “instalaciones empresariales”; a regiones alejadas de las grandes industrias de confección, en donde esta ocupación puede crecer significativamente y aportar a la microeconomía local. En relación con la problemática expuesta se plantea el técnico en Elaboración de prendas de vestir sobre medidas, ofreciendo alternativas de formación que faciliten el acceso a diferentes zonas del país y a una comunidad que ya está adaptada a una era digital, utilizando la tecnología como herramienta de aprendizaje de saberes técnicos y humanos. 
+    p.mb-4 Algunos sastres, modistas y diseñadores de moda han fundado academias independientes sin titulación acreditada (Inexmoda, 2021) tratando de cubrir la necesidad de aprendizaje de moldería sobre medidas que es más precisa, tal es el caso de Eometric, Pattercorns y el sistema de Donato Delego, mencionando solo algunos. Esto es debido a que las universidades, institutos y escuelas en su gran mayoría se han centrado en el patronaje industrial, dejando esta ocupación sin cualificación. 
+    p En el marco de este programa de formación el aprendiz podrá obtener las competencias necesarias para realizar el proceso técnico en la elaboración de prendas de vestir con calidad y eficiencia, interpretar diseños, orientar al cliente y calcular los costos de elaboración y de mano de obra. En concordancia, podrá emprender su propio proyecto respaldado por la política nacional de economía naranja, el acceso a participar en aceleradoras de PYMES en el país y/o pertenecer a empresas o casas de diseñadores independientes como muestrarista.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Competencias<br>a desarrollar
+    
+    ul.lista-ul
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Confeccionar prendas sobre medida según técnicas de sastrería y modistería.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Ejercer derechos fundamentales del trabajo en el marco de la Constitución Política y los convenios internacionales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Gestionar procesos propios de la cultura emprendedora y empresarial, de acuerdo con el perfil personal y los requerimientos de los contextos productivo y social.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales según los criterios establecidos por el Marco Común Europeo de Referencia para las Lenguas.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Modificar prendas de vestir de acuerdo con técnicas de sastrería y modistería.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de la información.
+
+  
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Perfil<br>de ingreso
+    
+    ul.lista-ul
+      li.d-flex  
+        i.fas.fa-angle-right
+        | Nivel académico media requerido: grado séptimo
+      li.d-flex  
+        i.fas.fa-angle-right
+        | Edad mínima: 14 años
+      li.d-flex  
+        i.fas.fa-angle-right
+        | Requisitos adicionales: cumplir con el proceso de selección establecido y conocimiento básico de manejo de máquina plana y fileteadora.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Perfil<br>de egreso
+  
+    p.mb-4 El técnico en elaboración de prendas de vestir sobre medidas está en la capacidad de interpretar modelos o diseño para iniciar el trabajo de confección, trazar y transformar moldes, cortar prendas de diferentes características, operar máquinas industriales, para realizar el ensamble y acabados de todo tipo de vestuario sobre medidas, con diferentes grados de complejidad.
+
+    p.mb-4 Puede desempeñarse de manera competente en un rango variado de la actividad de confección de prendas sobre medidas, como muestrarista en empresas de confección, dedicarse a modificar, arreglar y reparar prendas de vestir; confeccionar ropa de cama, ropa interior, manteles y otros productos de lencería.
+
+    p Está en condiciones de resolver problemas rutinarios simples, que se presentan durante la operación de las máquinas y durante el proceso de elaboración de moldes y corte. Es un egresado con habilidades organizativas y relaciones interpersonales, comprometido con la protección del medio ambiente y, la seguridad y salud en el trabajo.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Estrategia<br>metodológica
+
+    p.mb-4 Centrada en la construcción de autonomía para garantizar la calidad de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales, soportadas en la utilización de las Tecnologías de la Información y la Comunicación (TIC), integradas en ambientes abiertos y pluritecnológicos, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y  con el desarrollo de las competencias.
+
+    p.mb-4 Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje, que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    .row.g-4.justify-content-center
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center
+          strong Las TIC
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
